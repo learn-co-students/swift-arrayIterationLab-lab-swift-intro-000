90f5118f430cefcc3d8b12d4382154da5213b05a
hingyenyin <yenyin.262@gmail.com>
2016-10-23 22:20:32 +0800

    array3

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,7 +13,8 @@
 /*: question1
  ### 1. You're building an app and want to store all of the ingredients added to a cart. How would you represent this cart in code? The ingredients are "Chips", "Salsa", "Guacamole", "Red wine". Explicitly mark the content of the cart as strings.
  */
-// write your code here
+var ingredients: [String] = ["Chips", "Salsa", "Guacamole", "Red wine"]
+
 
 
 
@@ -23,7 +24,8 @@
 /*: question2
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code? Explicitly mark the content of the cart as numbers.
  */
-// write your code here
+var numbers: [Int] = [1,2,3,4,5,6,7,8,9,10]
+
 
 
 
@@ -34,7 +36,8 @@
 /*: question3
  ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible.
  */
-// write your code here
+print(numbers)
+
 
 
 
@@ -45,7 +48,8 @@
 /*: question4
  ### 4. Take the list of shopping cart items you created in question 1 and print their values in the most efficient way possible. Prefix each item in the array with a string of text.
  */
-// write your code here
+print(ingredients)
+
 
 
 
@@ -67,17 +71,54 @@
 /*: question6
  ### 4. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
-// write your code here
 
 
+func message (name: Array<String>) -> String {
+
+if name == "Michael"
+
+{ return "Top of the morning Michael"
 
 
+    }
+    
+else {
+    
+    "Good morning \(name)"
 
 
+    }
+    print(message(name: ["Michael"]))
+    
 /*: question7
  ### 7. Create a function that takes an array of Ints (40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49) and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
-// write your code here
+    func lessThanFifty (numbers: [Int]) -> Int {
+
+        var lessFifty = 0
+
+        for number in numbers {
+
+            if number <50 {
+
+                lessFifty +=1
+}
+
+}
+        
+        return lessFifty
+        
+    }
+
+let numbersFromArray = [40, 60, 50, 52, 59, 13, 90, 100, 5, 52, 51, 49]
+
+        
+        
+print(lessThanFifty(numbers: numbersFromArray))
+
+
+
+
 
 
 
