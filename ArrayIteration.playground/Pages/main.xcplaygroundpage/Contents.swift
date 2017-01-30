ac53cb8c5b12ead585637ba84fc302e955b7f20e
Michael Matthews <ipodappsrcool@gmail.com>
2017-01-29 19:55:19 -0500

    The gentle storm is a great band

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,7 +13,7 @@
 /*: question1
  ### 1. You're building an app and want to store all of the ingredients added to a cart into an `Array`. The ingredients are "Chips", "Salsa", "Guacamole", and "Red wine". Name this `Array` variable whatever you like, think of a name which makes sense.
  */
-// write your code here
+var cart = ["Chips","Salsa","Guacamole","Red wine"]// write your code here
 
 
 
@@ -23,7 +23,7 @@
 /*: question2
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code?
  */
-// write your code here
+let list = [1,2,3,4,5,6,7,8,9,10]// write your code here
 
 
 
@@ -34,7 +34,9 @@
 /*: question3
  ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible. (Hint: for-in loop).
  */
-// write your code here
+for number in list {
+    print(number)
+}// write your code here
 
 
 
@@ -45,7 +47,9 @@
 /*: question4
  ### 4. Take the list of shopping cart items you created in question 1 and iterate through it printing each item to the console.
  */
-// write your code here
+for item in cart {
+    print(item)
+}// write your code here
 
 
 
@@ -56,19 +60,38 @@
 /*: question5
  ### 5. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
-// write your code here
+func greeting(_ names: [String]) {
+    for name in names {
+        if name == "Michael" {
+            print("Top of the monring \(name)!")
+        } else {
+            print("Good morning \(name)")
+        }
+    }
+}// write your code here
 
+let test = ["Michael","Robert","Matthews"]
 
-
+greeting(test)
 
 
 
 /*: question6
  ### 6. Create a function that takes an array of Ints and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
-// write your code here
+func arrayShorteren(_ array: [Int]) -> [Int] {
+    var tempArray: [Int] = []
+    for number in array {
+        if number < 50 {
+            tempArray.append(number)
+        }
+    }
+    return tempArray
+}// write your code here
 
+let testArray = [49,50,51]
 
+print(arrayShorteren(testArray))
 
 
 //: Here is a [link](https://github.com/learn-co-curriculum/swift-arrayIterationLab-lab/blob/solution/ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift) to the solution.
