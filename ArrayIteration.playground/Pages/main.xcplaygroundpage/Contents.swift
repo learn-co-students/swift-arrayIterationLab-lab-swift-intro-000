69e192a10f37857e01e3540ba9bc2d1c847edadb
Lerrain <iskander2289@gmail.com>
2017-03-11 14:52:11 +0200

    12 lab done

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,64 +15,68 @@
  */
 // write your code here
 
-
-
-
-
+var iWannaIt: [String] = ["Chips", "Salsa", "Guacamole", "Red wine"]
 
 /*: question2
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code?
  */
 // write your code here
 
-
-
-
-
-
+var listOfNums: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 /*: question3
  ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible. (Hint: for-in loop).
  */
 // write your code here
 
-
-
-
-
-
+for num in listOfNums {
+	print(num)
+}
 
 /*: question4
  ### 4. Take the list of shopping cart items you created in question 1 and iterate through it printing each item to the console.
  */
 // write your code here
 
-
-
-
-
-
-
+for eat in iWannaIt {
+	print(eat)
+}
 /*: question5
  ### 5. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
 // write your code here
 
-
-
-
-
+func hiAll (persons: [String]) {
+	
+	for name in persons {
+		
+		print(name)
+		
+		if name == "Michael" {
+			print("Top of the morning Michael!")
+		} else {
+			print("Good morning \(name)")
+		}
+	}
+}
 
 /*: question6
  ### 6. Create a function that takes an array of Ints and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
 // write your code here
 
+func lessThenFiftyArray(numsArr: [Int]) -> [Int] {
 
+	var lessThenFifty: [Int] = []
+	
+	for num in numsArr {
+		
+		if num < 50 {
+			lessThenFifty.append(num)
+		}
+	}
+	return lessThenFifty
+}
 
 
 //: Here is a [link](https://github.com/learn-co-curriculum/swift-arrayIterationLab-lab/blob/solution/ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift) to the solution.
-
-
-
-
