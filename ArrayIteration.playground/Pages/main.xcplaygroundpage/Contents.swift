4c565a940192aa03500896f77ca42494c2239051
jcornelussen <j.cornelussen@planet.nl>
2017-05-02 23:13:02 +0200

    Done

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,7 @@
  ### 1. You're building an app and want to store all of the ingredients added to a cart into an `Array`. The ingredients are "Chips", "Salsa", "Guacamole", and "Red wine". Name this `Array` variable whatever you like, think of a name which makes sense.
  */
 // write your code here
-
+var shoppingList = ["Chips", "Sales", "Guacamole", "Red wine"]
 
 
 
@@ -24,9 +24,13 @@
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code?
  */
 // write your code here
+var list: [Int] = []
 
+for a in 1...10 {
+  list.append(a)
+}
 
-
+print(list)
 
 
 
@@ -35,10 +39,9 @@
  ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible. (Hint: for-in loop).
  */
 // write your code here
-
-
-
-
+for a in list {
+  print("\(a)")
+}
 
 
 
@@ -47,9 +50,9 @@
  */
 // write your code here
 
-
-
-
+for a in shoppingList {
+  print("Added item to cart: \(a)")
+}
 
 
 
@@ -58,19 +61,46 @@
  */
 // write your code here
 
+func goodMorning(person: [String]) {
+  for a in person {
+    switch a {
+    case "Michael":
+      print("Top of the morning Michael")
+    default:
+      print("Good morning \(a)")
+    }
+  }
+}
 
-
-
+let group = ["Hans", "Grietje", "Michael", "John"]
+goodMorning(person: group)
 
 
 /*: question6
  ### 6. Create a function that takes an array of Ints and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
 // write your code here
+func lessThanFifty(toSort: [Int]) -> [Int] {
+  var sorted = Array<Int>()
+  
+  for a in toSort {
+    if a < 50 {
+      sorted.append(a)
+    }
+  }
+  
+  return sorted
+}
 
+var unsorted = [78, 43, 23, 76, 15, 9, 12, 65, 76, 54, 49, 50, 10]
 
+var sorted = lessThanFifty(toSort: unsorted)
 
+print("Unsorted = \(unsorted)")
+print("Sorted = \(sorted)")
 
+//sorted = lessThanFifty(toSort: [])
+//print("Sorted = \(sorted)")
 //: Here is a [link](https://github.com/learn-co-curriculum/swift-arrayIterationLab-lab/blob/solution/ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift) to the solution.
 
 
