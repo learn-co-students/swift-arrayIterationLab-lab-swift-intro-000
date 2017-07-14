8d1e0ceeb3f7fde47845e361e7046a989f80e7cb
sonnguyen64 <son.nguyen0604@gmail.com>
2017-07-14 13:55:12 +0700

    Problem completed.

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,7 @@
  ### 1. You're building an app and want to store all of the ingredients added to a cart into an `Array`. The ingredients are "Chips", "Salsa", "Guacamole", and "Red wine". Name this `Array` variable whatever you like, think of a name which makes sense.
  */
 // write your code here
-
+var ingredients = ["Chips", "Salsa", "Guacamole", "Red wine"]
 
 
 
@@ -24,9 +24,11 @@
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code?
  */
 // write your code here
+var list: [Int] = []
 
-
-
+for i in 1...10 {
+    list.append(i)
+}
 
 
 
@@ -35,7 +37,9 @@
  ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible. (Hint: for-in loop).
  */
 // write your code here
-
+for number in list {
+    print(number)
+}
 
 
 
@@ -46,7 +50,9 @@
  ### 4. Take the list of shopping cart items you created in question 1 and iterate through it printing each item to the console.
  */
 // write your code here
-
+for ingredient in ingredients {
+    print(ingredient)
+}
 
 
 
@@ -57,7 +63,15 @@
  ### 5. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
 // write your code here
-
+func greeting(names: [String]) {
+    for name in names {
+        if name == "Michael" {
+            print("Top of the morning Michael!")
+        } else {
+            print("Good morning \(name)")
+        }
+    }
+}
 
 
 
@@ -67,7 +81,15 @@
  ### 6. Create a function that takes an array of Ints and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
 // write your code here
-
+func myFunction(array: [Int]) -> [Int] {
+    var result: [Int] = []
+    for number in array {
+        if number < 50 {
+            result.append(number)
+        }
+    }
+    return result
+}
 
 
 
