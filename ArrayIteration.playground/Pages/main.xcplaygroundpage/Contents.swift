17bcf815a81264debbd9b3d5eccbd19ded29b849
dgmalot <dgmalot@gmail.com>
2017-07-24 13:11:55 -0400

    Completd arrays lab.

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,7 +13,7 @@
 /*: question1
  ### 1. You're building an app and want to store all of the ingredients added to a cart into an `Array`. The ingredients are "Chips", "Salsa", "Guacamole", and "Red wine". Name this `Array` variable whatever you like, think of a name which makes sense.
  */
-// write your code here
+var cartIngredients = ["Chips", "Salsa", "Guacamole", "Red wine"]
 
 
 
@@ -23,10 +23,7 @@
 /*: question2
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code?
  */
-// write your code here
-
-
-
+let listNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 
@@ -34,6 +31,9 @@
 /*: question3
  ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible. (Hint: for-in loop).
  */
+for number in listNumbers {
+    print("\(number)")
+}
 // write your code here
 
 
@@ -45,9 +45,9 @@
 /*: question4
  ### 4. Take the list of shopping cart items you created in question 1 and iterate through it printing each item to the console.
  */
-// write your code here
-
-
+for ingredient in cartIngredients {
+    print("\(ingredient)")
+}
 
 
 
@@ -56,7 +56,19 @@
 /*: question5
  ### 5. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
-// write your code here
+func specialGreetings(nameList: [String]) {
+    for name in nameList {
+        switch name {
+        case "Michael":
+            print("Top of the morning \(name)")
+        default:
+            print("Good morning \(name)")
+        }
+    }
+}
+
+var friendsFromWork = ["Elisa", "Jayme", "Lily", "Michael", "Frankie", "Violet", "Schlomo"]
+specialGreetings(nameList: friendsFromWork)
 
 
 
@@ -66,9 +78,18 @@
 /*: question6
  ### 6. Create a function that takes an array of Ints and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
-// write your code here
+func isLessThan50(testNumbers: [Int]) -> [Int] {
+    var numbersLessThan50: [Int] = []
+    for number in testNumbers {
+        if number < 50 {
+            numbersLessThan50.append(number)
+        }
+    }
+    return numbersLessThan50
+}
 
-
+var listOfNumbers = [4, 78, 230, 23, 18, 37, 56, 893, 671, 71, 123, 2]
+print("\(isLessThan50(testNumbers: listOfNumbers))")
 
 
 //: Here is a [link](https://github.com/learn-co-curriculum/swift-arrayIterationLab-lab/blob/solution/ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift) to the solution.
