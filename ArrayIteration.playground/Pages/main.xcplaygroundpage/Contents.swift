a2d8fc8c7919901ff5b21cebaeafc92424361e8a
regina02 <rlarcilla@hotmail.com>
2017-08-18 15:20:03 -0700

    finished lab

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,6 +15,8 @@
  */
 // write your code here
 
+var groceryCart = ["Chips", "Salsa", "Guacamole", "Red wine"]
+
 
 
 
@@ -23,7 +25,7 @@
 /*: question2
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code?
  */
-// write your code here
+var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 
@@ -34,8 +36,9 @@
 /*: question3
  ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible. (Hint: for-in loop).
  */
-// write your code here
-
+for number in list {
+    print(number)
+}
 
 
 
@@ -45,7 +48,9 @@
 /*: question4
  ### 4. Take the list of shopping cart items you created in question 1 and iterate through it printing each item to the console.
  */
-// write your code here
+for ingredient in groceryCart{
+    print(ingredient)
+}
 
 
 
@@ -56,17 +61,33 @@
 /*: question5
  ### 5. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
-// write your code here
-
-
-
-
+func greetEveryone(names: [String]){
+    for person in names{
+        if (person == "Michael"){
+            print("Top of the morning Michael!")
+        }
+        else{
+            print("Good morning \(person)")
+        }
+    }
+    
+}
 
 
 /*: question6
  ### 6. Create a function that takes an array of Ints and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
-// write your code here
+func lessThan50 (array : [Int])-> [Int]{
+    var newArray : [Int] = []
+    
+    for number in array{
+        if (number < 50){
+            newArray.append(number)
+        }
+    }
+    
+    return newArray
+}
 
 
 
