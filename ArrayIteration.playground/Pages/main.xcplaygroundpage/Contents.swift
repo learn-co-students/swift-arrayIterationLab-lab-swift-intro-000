3692fdb5bfeea4801c2f1db06cf10a457cd9d3ca
deranp <deran.p@gmail.com>
2018-03-13 14:48:34 +0200

    Completed Playground

--- ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,62 +15,79 @@
  */
 // write your code here
 
-
-
-
-
-
+var shoppingCart: [String] = []
+shoppingCart = ["Chips","Guacamole","Red wine"]
 /*: question2
  ### 2. You need to create a list that contains the numbers from 1 to 10. How would you represent this list in code?
  */
 // write your code here
 
-
-
-
-
-
+let numberList: [Int] = [1,2,3,4,5,6,7,8,9,10]
 
 /*: question3
  ### 3. Take the list of numbers you created in question 2 and print their values in the most efficient way possible. (Hint: for-in loop).
  */
 // write your code here
 
-
-
-
-
-
+for number in numberList
+{
+    print (number)
+}
 
 /*: question4
  ### 4. Take the list of shopping cart items you created in question 1 and iterate through it printing each item to the console.
  */
 // write your code here
 
-
-
-
-
-
+for item in shoppingCart
+{
+    print (item)
+}
 
 /*: question5
  ### 5. Create a function that takes an array of names and greets each person with the following message "Good morning x" <- x being the individuals name. One caveat. If the persons first name is Michael, we want to greet them with the message, "Top of the morning Michael!".
  */
 // write your code here
 
+func showGreeting(charNames: [String])
+{
+    for name in charNames
+    {
+        if name == "Michael"
+        {
+            print ("Top of the morning \(name)")
+        }
+        else
+        {
+            print ("Good morning \(name)")
+        }
+    }
+}
 
-
-
-
+let names: [String] = ["Deran","Keshni","Saiyen","Michael","Kelly","Gordan"]
+showGreeting(charNames: names)
 
 /*: question6
  ### 6. Create a function that takes an array of Ints and returns an array of Ints. This function should look through the array of Ints and create a new array of Ints that only contain integers less than 50.
  */
 // write your code here
+func lessThanFifty(numbers: [Int]) -> [Int]
+{
+    var newNumbers:[Int] = []
+    
+    for number in numbers
+    {
+        if number < 50
+        {
+            newNumbers.append(number)
+        }
+    }
+    return newNumbers
+}
 
-
-
-
+let oldNumbers: [Int] = [10,20,30,40,49,50,60,10,70,100,101,1,2,3,4,5]
+let newNumber: [Int] = lessThanFifty(numbers: oldNumbers)
+print (newNumber)
 //: Here is a [link](https://github.com/learn-co-curriculum/swift-arrayIterationLab-lab/blob/solution/ArrayIteration.playground/Pages/main.xcplaygroundpage/Contents.swift) to the solution.
 
 
